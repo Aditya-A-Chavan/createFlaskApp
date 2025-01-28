22a4424d62696a3d7cbde051e44424cd1bc91cd4
Aditya Chavan <130660675+Aditya-A-Chavan@users.noreply.github.com>
2025-01-28 22:24:56 +0530

    ini

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
-print("HELLO WORLD I AM BOT")
+import argparse
+from createFlaskAppBackend.main import test
+import os
+
+def main():
+    parser = argparse.ArgumentParser(description="A bot that renames files in a directory based on their content.")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose output (show filenames during renaming)")
+
+    args = parser.parse_args()
+    current_directory = os.getcwd()
+
+    if args.verbose:
+        print("Verbose mode is enabled.")
+
+    test()
+
+if __name__ == "__main__":
+    main()
