be79e02eac47e6e7fa0febfc9d993ccc54cdf0e1
Aditya Chavan <130660675+Aditya-A-Chavan@users.noreply.github.com>
2025-01-31 00:11:56 +0530

    dir and file creation

--- bot.py
+++ bot.py
@@ -1,18 +1,32 @@
 import argparse
-from createFlaskAppBackend.main import test
+from createFlaskAppBackend.main import main1
 import os
 
 def main():
-    parser = argparse.ArgumentParser(description="A bot that renames files in a directory based on their content.")
-    parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose output (show filenames during renaming)")
+    parser = argparse.ArgumentParser(description="A bot that creates flask app directory structure and files along with boilerplate code.")
+    parser.add_argument("-docker", action="store_true", help="Include Dockerfile and docker-compose configuration.")
+    parser.add_argument("-port", type=int, help="Port number for the flask app.", dest="port", default=8080)
+    parser.add_argument("-extraDirs", nargs="+", help="Extra directories to create.")
 
     args = parser.parse_args()
-    current_directory = os.getcwd()
+    currDirs = os.getcwd()
+    
+    docker = False
+    if args.docker:
+        docker = True
+        print("docker mode is enabled.")
+    
+    if args.port:
+        print(f"Port number is {args.port}")
+    
+    extraDirs = None
+    if args.extraDirs:
+        extraDirs = args.extraDirs
+        print(f"Extra directories are {args.extraDirs}")
+    
+    main1(extraDirs, currDirs, docker)
 
-    if args.verbose:
-        print("Verbose mode is enabled.")
-
-    test()
+    print(f"Current directory is {currDirs}")
 
 if __name__ == "__main__":
     main()
--- createFlaskAppBackend/main.py
+++ createFlaskAppBackend/main.py
@@ -1,2 +1,36 @@
-def test():
-    return "Hello World"
+import os
+
+def createDirs(currDir, extraDirs=None):
+    dirs = ["app", "nginx", "app/authentication", "app/utils", "app/toolbox"]
+    
+    
+    if extraDirs:
+        for dir in extraDirs:
+            dirs.append(dir)
+
+    for dir in dirs:
+        os.makedirs(os.path.join(currDir, dir), exist_ok=True)
+        print(f"Directory {dir}, created successfully.")
+
+    print("\nAll Directories created successfully.")
+
+def createFiles(currDir, docker=False):
+    files = ["Makefile", "Manage.py", "app/__init__.py", "app/config-common.py", "app/config-dev.py", "app/config-prod.py", "app/logger-setup.py", "nginx/Dockerfile", "app/authentication/__init__.py", "app/utils/__init__.py", "app/utils/errorHandler.py", "app/toolbox/__init__.py"]
+    
+    if docker:
+        files.append("Dockerfile")
+        files.append(".dockerignore")
+        files.append("docker-compose.yml")
+
+    for file in files:
+        with open(os.path.join(currDir, file), "w") as f:
+            f.write("# Created by createFlaskAppBackend\n")
+            print(f"File {file}, created successfully.")
+
+    print("\nAll Files created successfully.")
+    
+
+def main1(extraDirs, currDir, docker, dockerCompose):
+    createDirs(currDir, extraDirs)
+    createFiles(currDir, docker, dockerCompose)
+
